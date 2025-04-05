eabdcc17a172e8269b79069e6458a0a563ceae00
ignaciosica <mignacio.sica@gmail.com>
2025-04-05 19:16:56 +0800

    revert change to tc=3

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -627,7 +627,7 @@ class Kernel:
 
             if self.use_tensor_cores == 3:  # for TC=3, emulate the warp addressing with locals
               local_shape = tuple(1 if st == 0 or i < wd or (i >= self.first_reduce and i < tcd) else src_st.shape[i] \
-                                  for i,st in enumerate(src_st.real_strides(True)))
+                                  for i,st in enumerate(src_st.real_strides()))
               st = store_st = ShapeTracker.from_shape(local_shape)
               local_buffer = UOp(Ops.DEFINE_LOCAL, tc.dtype_in.ptr(size=st.real_size(), local=True), (), f"temp{i}")
               if swizzle: store_st = get_tc_swizzle_st(store_st.shape, *swizzle)
