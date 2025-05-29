e276943186c678e2e571f970b600faf29c8d4155
ignaciosica <mignacio.sica@gmail.com>
2025-05-29 19:33:16 -0300

    format ruff

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -455,33 +455,34 @@ class Kernel:
 
     print(f"Buf [{(buf := uop.src[0]).arg}] (op: {'st' if uop.op is Ops.STORE else 'ld'} {'global' if buf.op is Ops.DEFINE_GLOBAL else 'shared'})")
     st = uop.st_arg
-    st = st.shrink(tuple((0, 1) if i < self.global_dims else (0, s) for i,s in enumerate(st.shape)))                                        # noqa:E501 shrink global dims
-    st = st.shrink(tuple((0, 1) if self.first_reduce <= i < self.first_upcast else (0, s) for i,s in enumerate(st.shape)))                  # noqa:E501 shrink reduce dims
-    st = st.shrink(tuple((0, 1) if (self.first_upcast <= i and s == 0) else (0, st.shape[i]) for i,s in enumerate(st.real_strides(True))))  # noqa:E501 shrink broadcasted upcast dims
-    st = st.expand(tuple(self.full_shape[i] if self.global_dims <= i < self.local_dims else s for i,s in enumerate(st.shape)))              # noqa:E501 expand local dims
+    st = st.shrink(tuple((0, 1) if i < self.global_dims else (0, s) for i, s in enumerate(st.shape)))  # noqa:E501 shrink global dims
+    st = st.shrink(tuple((0, 1) if self.first_reduce <= i < self.first_upcast else (0, s) for i, s in enumerate(st.shape)))  # noqa:E501 shrink reduce dims
+    st = st.shrink(tuple((0, 1) if (self.first_upcast <= i and s == 0) else (0, st.shape[i]) for i, s in enumerate(st.real_strides(True))))  # noqa:E501 shrink broadcasted upcast dims
+    st = st.expand(tuple(self.full_shape[i] if self.global_dims <= i < self.local_dims else s for i, s in enumerate(st.shape)))  # noqa:E501 expand local dims
 
     tile_strides = canonicalize_strides(st.shape, tuple(itertools.accumulate(st.shape, operator.mul, initial=1)))
     tile_st = ShapeTracker((View.create(shape=st.shape, strides=tile_strides),))
 
     layout: dict = {}
-    print(f"{uop.st_arg.size=} {uop.st_arg.real_size()=} {st.size=} {st.real_size()=} {tile_st.size=} {tile_st.real_size()=}")
-    print(f"{uop.st_arg}\n{st}\n{tile_st}")
+    # print(f"{uop.st_arg.size=} {uop.st_arg.real_size()=} {st.size=} {st.real_size()=} {tile_st.size=} {tile_st.real_size()=}")
+    # print(f"{uop.st_arg}\n{st}\n{tile_st}")
     for i in range(0, tile_st.real_size()):
       logical_coords: tuple[UOp, ...] = tuple(sint_to_uop(c) for c in unravel(tile_st.shape, i))
       idx, idx_valid = st.to_indexed_uops(logical_coords)
       tile_idx, tile_valid = tile_st.to_indexed_uops(logical_coords)
-      if idx_valid.arg and tile_valid.arg: layout.setdefault(idx.arg, []).append(tile_idx.arg)
+      if idx_valid.arg and tile_valid.arg:
+        layout.setdefault(idx.arg, []).append(tile_idx.arg)
 
     matrix, elems, tidx, RESET = None, [], getenv("TIDX", -1), "\x1b[0m"
     local_size = prod(s for s in st.shape[self.global_dims : self.first_reduce])
     for i, coords in sorted(layout.items()):
-      ths = tuple(set(cs %  local_size for cs in coords))
-      ups = tuple(set(cs // local_size for cs in coords))
-      # print(f"T({ths})[{ups}]")
-      elems += [f"{ansi(ths[0]) if tidx==-1 else ansi(5) if tidx in ths else RESET}T({','.join(str(f'{t:02d}') for t in ths)})[{ups[0]:02d}]{RESET}"]
+      ts = tuple(set(cs % local_size for cs in coords))
+      us = tuple(set(cs // local_size for cs in coords))
+      # print(f"T({ts})[{us}]")
+      elems += [f"{ansi(ts[0]) if tidx == -1 else ansi(5) if tidx in ts else RESET}T({','.join(str(f'{t:02d}') for t in ts)})[{us[0]:02d}]{RESET}"]
 
     width = 32 * 4 // buf.dtype.itemsize if buf.op is Ops.DEFINE_LOCAL else 8
-    matrix = [elems[i:i+width] for i in range(0,len(elems), width)]
+    matrix = [elems[i : i + width] for i in range(0, len(elems), width)]
     print(tabulate(matrix or [elems], tablefmt="simple_grid", maxcolwidths=11, showindex=True, headers=tuple(i for i in range(width))))
     del layout
 
