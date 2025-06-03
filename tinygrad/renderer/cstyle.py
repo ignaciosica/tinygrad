27133cb4093ad154200e3ca9e767dc1b0591bac2
ignaciosica <mignacio.sica@gmail.com>
2025-06-03 19:20:48 -0300

    remove clutter in output kernel

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -288,7 +288,7 @@ class MetalRenderer(CStyleLanguage):
   # language options
   kernel_prefix = "kernel "
   buffer_prefix = "device "
-  smem_prefix = "threadgroup __attribute__((aligned(16))) "
+  smem_prefix = "threadgroup "
   arg_int_prefix = "constant int&"
   barrier = "threadgroup_barrier(mem_flags::mem_threadgroup);"
   float4 = "float4"
