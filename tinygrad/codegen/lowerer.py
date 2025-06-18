957e85be330adc81346635baa95a5652baec64ae
ignaciosica <mignacio.sica@gmail.com>
2025-06-18 13:55:17 -0300

    split lower_store into global and local

--- tinygrad/codegen/lowerer.py
+++ tinygrad/codegen/lowerer.py
@@ -142,16 +142,22 @@ def lower_load(ctx: IndexContext, x: UOp, buf: UOp):
   barrier = (UOp(Ops.BARRIER, dtypes.void, (x.src[1],)),) if buf.op is Ops.DEFINE_LOCAL else ()
   return UOp(Ops.LOAD, x.dtype, (buf.index(idx, valid),) + barrier)
 
-def lower_store(ctx: IndexContext, x: UOp, buf: UOp):
+def lower_global_store(ctx: IndexContext, x: UOp, buf: UOp):
+  idx, valid = x.st_arg.to_indexed_uops(ctx.idxs)
+  for oidx, ridx in zip(ctx.idxs, ctx.ridxs):
+    if oidx is not ridx: valid = valid * oidx.eq(0)
+  return UOp(Ops.STORE, dtypes.void, (buf.index(idx, valid), x.src[1]))
+
+def lower_local_store(ctx: IndexContext, x: UOp, buf: UOp):
   idx, valid = x.st_arg.to_indexed_uops(ctx.idxs)
   # NOTE: only store the local reduceop in the threads that are actually doing the reduce
-  if cast(PtrDType, buf.dtype).local and x.src[1].op is Ops.REDUCE:
+  if x.src[1].op is Ops.REDUCE:
     reduce_input = x.src[1].src[0]
     store_back = reduce_input.op is Ops.LOAD and cast(PtrDType, reduce_input.src[0].dtype).local
   else: store_back = False
   # NOTE: If we're storing the reduced value back into each thread, need to zero-out the reduced axes
   if store_back: idx, _ = x.st_arg.to_indexed_uops([u.const_like(0) if u in x.src[1].src else u for u in ctx.idxs])
-  if (not cast(PtrDType, buf.dtype).local) or store_back:
+  if store_back:
     for oidx, ridx in zip(ctx.idxs, ctx.ridxs):
       if oidx is not ridx: valid = valid * oidx.eq(0)
   return UOp(Ops.STORE, dtypes.void, (buf.index(idx, valid), x.src[1]))
@@ -166,7 +172,8 @@ pm_lowerer = PatternMatcher([
   (UPat(Ops.VALID, src=(UPat(Ops.VIEW),), name="x"), lambda ctx,x: x.st_arg.to_indexed_uops(ctx.idxs)[1]),
   # rewrite LOAD/STORE VIEW to LOAD/STORE with indexed
   (UPat(Ops.LOAD, src=(UPat.var("buf").view(),), allow_any_len=True, name="x"), lower_load),
-  (UPat(Ops.STORE, src=(UPat.var("buf").view(),), allow_any_len=True, name="x"), lower_store),
+  (UPat(Ops.STORE, src=(UPat(Ops.DEFINE_LOCAL, name="buf").view(),), allow_any_len=True, name="x"), lower_local_store),
+  (UPat(Ops.STORE, src=(UPat(Ops.DEFINE_GLOBAL, name="buf").view(),), allow_any_len=True, name="x"), lower_global_store),
   (UPat(Ops.INDEX, src=(UPat.var("b"), UPat.var("idx"), UPat.const(dtypes.bool, True))), lambda b, idx: b.index(idx)),
 ])
 
